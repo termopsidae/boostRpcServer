64957975a2693a6e6aab31558573e50b043a68a4
zhcyueliyangzi <yueliyangzi@163.com>
2023-05-23 15:30:49 +0800

    first commit

--- remix/collection-erc721/IERC165.sol
+++ remix/collection-erc721/IERC165.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC721Metadata {
-    function name() external view returns (string memory);
 
-    function symbol() external view returns (string memory);
+interface IERC165 {
 
-    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
